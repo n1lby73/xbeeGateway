1c861789026ae24a725ac1768065b2f2b3004f3d
n1lby73 <n1lby73@gmail.com>
2025-04-17 16:47:19 +0100

    updated variables modules

--- modules/variables.py
+++ modules/variables.py
@@ -1,1 +1,6 @@
-prefferedRadioSerialNumber = "SER=AQ016E77"
+# prefferedRadioSerialNumber = "SER=AQ016E77"
+prefferedRadioSerialNumber = "SER=AQ015ZB9"
+xbeeBaudRate = 9600
+knownXbeeAddress = []
+xbeeAddressModbusMap = {}
+nextModbusAddressStart = 50
