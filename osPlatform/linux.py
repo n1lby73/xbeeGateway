faabcc831a2efe228cf290aede504e3b440eec05
n1lby73 <n1lby73@gmail.com>
2025-04-17 04:36:37 +0100

    ability to receive msg from radio

--- osPlatform/linux.py
+++ osPlatform/linux.py
@@ -1,15 +1,49 @@
-from xbee import XBee
+import sys
+import time
+# from xbee import XBee
+from digi.xbee.devices import XBeeDevice
 from modules.serialSelector import selectUsbPort
+from modules.xbeeData import getNodeId
 from modules.variables import xbeeBaudRate
 
 serialPort = selectUsbPort()
+baudRate = 9600
 
-print (serialPort)
 
-# def xbeePolling():
+if not serialPort:
 
-#     try:
+    print ("Gateway radio not connected")
 
-#         xbee = x
+    sys.exit(1)
 
-#     except:
+def xbeePolling():
+
+    try:
+
+        xbee = XBeeDevice(serialPort, baudRate)
+        xbee.open()
+
+        def dataReceiveCallback(xbeeMessage):
+
+            xbeeMacAddress = xbeeMessage.remote_device.get_64bit_addr()
+            xbeeRemoteDevice = xbeeMessage.remote_device
+            xbeeNodeIdentifier = xbeeMessage.remote_device.get_node_id()
+            timestamp = xbeeMessage.timestamp
+            data = xbeeMessage.data
+
+            print("Received data from %s: %s %s" % (xbeeMacAddress, xbeeNodeIdentifier, data))
+
+        xbee.add_data_received_callback(dataReceiveCallback)
+
+        while True:
+            # This is to make sure that the data is open and awaiting data
+            time.sleep(1) 
+
+    except Exception as e:
+
+        print (e)
+
+        pass
+
+if __name__ == "__main__":
+    xbeePolling()
