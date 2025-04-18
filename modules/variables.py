eae42a6d02a8bb47c3f2a0f2b47194ee631dfeb6
n1lby73 <n1lby73@gmail.com>
2025-04-18 07:56:42 +0100

    added new variables

--- modules/variables.py
+++ modules/variables.py
@@ -1,6 +1,8 @@
 # prefferedRadioSerialNumber = "SER=AQ016E77"
 prefferedRadioSerialNumber = "SER=AQ015ZB9"
 xbeeBaudRate = 9600
+xbeeData = None
 knownXbeeAddress = []
 xbeeAddressModbusMap = {}
 nextModbusAddressStart = 50
+sensorValues = []
