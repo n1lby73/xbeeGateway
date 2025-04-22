7eb10903765e86e17547c95e71a963708e6860c0
n1lby73 <n1lby73@gmail.com>
2025-04-22 10:00:29 +0100

    working on modbus server and polling logic

--- osPlatform/linux.py
+++ osPlatform/linux.py
@@ -1,4 +1,4 @@
-import sys, time
+import sys, time, asyncio
 # from xbee import XBee
 from datetime import datetime
 from digi.xbee.devices import XBeeDevice
@@ -7,8 +7,15 @@ from modules.serialSelector import selectUsbPort
 from modules.xbeeData import cayenneParse
 from modules.modbus import floatToRegisters, contextManager
 from modules import variables
+from pymodbus.server import StartAsyncTcpServer
+from pymodbus.device import ModbusDeviceIdentification
 
+# Async queue to store incoming packets
+xbeeQueue = asyncio.Queue() # Stores recieved mac address and data temporarily for processing
+# xbeeDataMacLock = asyncio.Lock()
 serialPort = selectUsbPort()
+
+
 
 if not serialPort:
 
@@ -16,7 +23,8 @@ if not serialPort:
 
     sys.exit(1)
 
-def xbeePolling():
+# Async wrapper for polling XBee data and placing into queue
+async def xbeePolling():
 
     try:
 
@@ -25,29 +33,29 @@ def xbeePolling():
 
         def dataReceiveCallback(xbeeMessage):
 
-            xbeeMacAddress = xbeeMessage.remote_device.get_64bit_addr()
-            # xbeeRemoteDevice = xbeeMessage.remote_device
-            # xbeeNodeIdentifier = getNodeId(xbeeRemoteDevice, xbeeMacAddress, xbee)
+            xbeeMacAddress = str(xbeeMessage.remote_device.get_64bit_addr())
             timestamp = datetime.fromtimestamp(xbeeMessage.timestamp)
-            variables.xbeeDataAsByte = xbeeMessage.data
+            xbeeDataAsByte = xbeeMessage.data
+            macIsNew = 0 # This value is used to denote if it's a newly found mac address so that cayenne parse would not need to query the xbeeMacAndDataMap before knowing if it's to update data or insert a new value
 
             if str(xbeeMacAddress) not in variables.knownXbeeAddress:
 
                 print (f"\nNew XBee Address Discovered: {xbeeMacAddress}")
 
                 variables.knownXbeeAddress.append(str(xbeeMacAddress))
+                macIsNew = 1
 
-                print (f"List of addresses discovered so far is: {variables.knownXbeeAddress}\n")
+                print (f"List of addresses discovered so far are: {variables.knownXbeeAddress}\n")
 
-            print(f"Received data from {xbeeMacAddress} @ {timestamp}: {variables.xbeeDataAsByte}\n")
-
-            cayenneParse(str(xbeeMacAddress), variables.xbeeDataAsByte)
+            print(f"Received data from {xbeeMacAddress} @ {timestamp} are: {xbeeDataAsByte}\n")
+            xbeeQueue.put_nowait((xbeeMacAddress, xbeeDataAsByte, macIsNew))
+            # cayenneParse(str(xbeeMacAddress), variables.xbeeDataAsByte, macIsNew)
 
         xbee.add_data_received_callback(dataReceiveCallback)
 
         while True:
             # This is to make sure that the data is open and awaiting data
-            time.sleep(1) 
+            await asyncio.sleep(1) 
 
     except Exception as e:
 
@@ -61,9 +69,82 @@ def xbeePolling():
 
             xbee.close()
 
-def modbusPolling(contextValue):
+async def modbusPolling(contextValue):
+
+    # nextModbusAddressStart = 0
 
 
+    while True:
+
+        mac, raw_data, ismac = await xbeeQueue.get()
+
+        try:
+            cayenneParse(mac, raw_data, ismac)
+
+            for macAddress, macData in variables.xbeeMacAndDataMap.items():
+
+                sensorValues = macData.get("sensorValues", [])
+
+                 # Assign a block if first time
+                if macAddress not in variables.xbeeAddressModbusMap:
+                    variables.xbeeAddressModbusMap[macAddress] = variables.nextModbusAddressStart
+                    variables.nextModbusAddressStart += 50
+
+                # Convert floats to register values
+                register_values = []
+                for val in sensorValues:
+                    register_values.extend(floatToRegisters(val))
+
+                # Limit to 20 registers (10 floats)
+                regs = register_values[:20]
+                start_addr = variables.xbeeAddressModbusMap[macAddress]
+
+                # Write to Holding (FC3) and Input (FC4) registers
+                # contextValue.setValues(3, start_addr, regs)
+                # contextValue.setValues(4, start_addr, regs)
+                for i in contextValue:
+                    print (i)
+                slave = contextValue.getSlaveContext(0)
+                slave.setValues(3, start_addr, regs)
+                slave.setValues(4, start_addr, regs)
+
+        except Exception as e:
+            
+            print(f"Modbus polling error: {e}")
+
+        finally:
+            xbeeQueue.task_done()
+        # time.sleep(1)
+
+
+
+async def modbusServer(context):
+
+    identity = ModbusDeviceIdentification()
+    identity.VendorName = 'XbeeGateway'
+    identity.ProductCode = 'XBEE'
+    identity.VendorUrl = 'http://yourdomain.com'
+    identity.ProductName = 'Modbus TCP Xbee Gateway'
+    identity.ModelName = 'ModbusTCPv1'
+    identity.MajorMinorRevision = '1.0'
+
+    print("Starting Modbus TCP server on port 5020...")
+    await StartAsyncTcpServer(context, identity=identity, address=("0.0.0.0", 5020))
+
+
+# if __name__ == "__main__":
+#     await asyncio.gather(
+#         xbeePolling())
+
+# Main entry
+async def main():
+    context = contextManager()
+    await asyncio.gather(
+        xbeePolling(),
+        modbusPolling(context),
+        modbusServer(context)
+        # process_data(context),
+    )
 
 if __name__ == "__main__":
-    xbeePolling()
+    asyncio.run(main())
