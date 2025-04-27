3c3b1858d8c25bfd933678ad64abc0285a958e42
n1lby73 <n1lby73@gmail.com>
2025-04-27 05:56:48 +0100

    configure xbee radios

--- modules/dbIntegration.py
+++ modules/dbIntegration.py
@@ -1,4 +1,6 @@
 import pymongo
+import json
+from . import variables
 
 dbclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
@@ -19,9 +21,39 @@ def dbQueryModbusStartAddress(macAddress):
 
         return None
 
+def configureXbeeModbusStartAddress(macAddress, startAddress, nodeIdentifier):
 
+    try:
 
+        validateUniqueMacAddress = modbusStartAddressCollectioin.find_one({"xbeeMac":macAddress})
+        validateStartAddress = modbusStartAddressCollectioin.find_one({"modbusStartAddress":startAddress})
 
-mydict = { "xbeeMac": "123456", "modbusStartAddress": "0" }
+        if validateUniqueMacAddress or validateStartAddress:
 
-x = mycol.insert_one(mydict)
+            xbeeStartAddress = validateUniqueMacAddress["modbusStartAddress"]
+            xbeeNodeId = validateUniqueMacAddress["xbeeNodeIdentifier"]
+
+            return json.dumps({"error":f"Mac address ({macAddress}) already configured with start address as {xbeeStartAddress} and node identifier as {xbeeNodeId} "})
+
+        # Validate that specified modbus address is not in between two xbee device
+
+        lastData = modbusStartAddressCollectioin.find_one(sort=[("_id", -1)])
+        retrieveLastConfiguredAddress = lastData["modbusStartAddress"]
+
+        validAvailableModbusAddress = int(retrieveLastConfiguredAddress) + variables.incrementalModbusAddress
+
+        if validAvailableModbusAddress < startAddress: # would still need to work on this to make it more robust and stay safe of memory gap
+
+            print('selected memory map would cause adress overlapping')
+
+            return json.dumps({"error":f"next availiable start address is {validAvailableModbusAddress}"})
+
+        xbeeData = {"xbeeMac":macAddress, "modbusStartAddress":startAddress, "xbeeNodeIdentifier":nodeIdentifier}
+
+        modbusStartAddressCollectioin.insert_one(xbeeData)
+
+        return json.dumps({"success":xbeeData})
+    
+    except Exception as e:
+
+        return json.dumps({"error":e})
